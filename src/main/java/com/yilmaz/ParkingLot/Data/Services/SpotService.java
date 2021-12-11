d2a0694d4e3fef9ca8365f0273f6b0962f86f64c
Yılmaz Özsoy <yilmaz.ozsoy@halkbank.com.tr>
2021-12-12 02:51:58 +0300

    small refactor

--- src/main/java/com/yilmaz/ParkingLot/Data/Services/SpotService.java
+++ src/main/java/com/yilmaz/ParkingLot/Data/Services/SpotService.java
@@ -2,11 +2,6 @@ package com.yilmaz.ParkingLot.Data.Services;
 
 import com.yilmaz.ParkingLot.Model.Spot;
 
-import java.util.List;
-
 public interface SpotService extends  CrudService<Spot, Long>{
 
-//    Owner findByLastName(String lastName);
-
-//    List<Owner> findAllByLastNameLike(String lastName);
 }
