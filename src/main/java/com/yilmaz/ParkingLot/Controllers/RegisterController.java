25140737f68167d43974fae297a6724ebeb4f774
Yılmaz Özsoy <yilmaz.ozsoy@halkbank.com.tr>
2021-12-12 02:41:49 +0300

    small refactor

--- src/main/java/com/yilmaz/ParkingLot/Controllers/RegisterController.java
+++ src/main/java/com/yilmaz/ParkingLot/Controllers/RegisterController.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
